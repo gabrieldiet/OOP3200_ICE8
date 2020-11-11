cd04bb70c81889c2d65e238f774d89ba800047f0
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-11-11 10:32:19 -0500

    BAsic input and output test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
+/*
+Author: Gabriel Dietrich
+Date: Nov 11, 2020
+ */
+
+
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s", name);
     }
 }
