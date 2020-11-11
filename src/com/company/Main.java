494a0e2dbe7159ef5950a4db17414f475a5b4193
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-11-11 11:28:57 -0500

    Added validation for the users age

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ Date: Nov 11, 2020
 
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -25,11 +26,39 @@ public class Main
 
         // Age
         float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
-        System.out.println();
+        boolean isValidInput = false; // Sentinel variable
+
+        do
+        {
+            try
+            {
+                System.out.print("Please enter your age: ");
+                age = keyboard.nextFloat();
+                isValidInput = true;
+
+                // Makes sure age is above 0 and below 120
+                if((age <= 0.0f) || (age >= 120.0f))
+                {
+                    System.out.println("Error: You must enter an age greater than 0.0 and less than 120.0 years old");
+                    keyboard.nextLine();
+                    isValidInput = false;
+                }
+
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating point number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+
+        }
+        while(!isValidInput);
+
+
 
         //Output
+        System.out.println();
         System.out.printf("You entered: %s \n", name);
         System.out.printf("You entered: %.2f \n", age);
     }
